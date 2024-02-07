b748e534d0ebe6eff827c3ee2ad1f7ec8229cae3
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-02-07 14:02:48 +0100

    unify godot df and process df

--- merge-and-plot.py
+++ merge-and-plot.py
@@ -5,11 +5,51 @@ import sys
 tolerance = 0
 
 def unify_views(godot_df, process_df):
+    #original idea: create df with all timestamps between min and max in micros
+    #join the other two dfs onto that
+    #PROBLEM: process crashes due to memory overload
+    #process_timestamps = process_df.index
+    #print(process_timestamps)
+    #timestamp_min = process_timestamps.min()
+    #timestamp_max = process_timestamps.max()
+    #print(str(timestamp_min) + " " + str(timestamp_max))
+    #all_timestamps = pd.Series(range(timestamp_min, timestamp_max, 1))
+    #print(all_timestamps)#debug
+    #unified_views_df=pd.DataFrame(index=all_timestamps)
+    #print(unified_views_df)#debug
+    #join_list = [godot_df, process_df]
+    #unified_views_df.join(join_list, how="left")
+    #print(unified_views_df)#debug
+
+    #simplified approach: divide indexes by 1000000 and join dfs directly
+    #at cost of subsecond precision
+    float_process_seconds_index = process_df.index / 1000000
+    process_seconds_index = float_process_seconds_index.astype("int")
+    float_godot_seconds_index = godot_df.index / 1000000
+    godot_seconds_index = float_godot_seconds_index.astype("int")
+    #print(process_seconds_index)#debug
+    #print(godot_seconds_index)#debug
+
+    godot_new = godot_df.set_index(godot_seconds_index)
+    process_new = process_df.set_index(process_seconds_index)
+    print(godot_new)
+    print(process_new)
+    unified_views_df=process_new.join(godot_new, how="left")
+    print(unified_views_df)
+
+
+
+
     #create timestamp in micro seconds series with range from monitor_df
     #create new unified df with series as index
     #join/merge godot_df and monitor df onto new df
     # return new df
-    pass
+    return unified_views_df
+
+def to_int_seconds(value):
+    second = value / 1000000
+    int_second = int(second)
+    return int_second
 
 def plot_unified_views_df(unified_views_df):
     #plot
@@ -89,6 +129,8 @@ def main():
     gds_process_rows=gds_process_df.shape[0]
 
     print("happy plotting!")
+
+    unified_views = unify_views(rust_godot_df, rust_process_df)
 
 
 
